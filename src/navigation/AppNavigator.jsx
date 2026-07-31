97a9fa5710044a05838713c93e57ba1da53eb81a
Elemento5-2026 <elemento5ag2026@outlook.com>
2026-07-31 13:20:17 -0600

    Update AppNavigator.jsx

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -1,15 +1,5 @@
-import { Text, View } from "react-native";
+import Login from "../pages/Login/Login";
 
 export default function AppNavigator() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Elemento 5 - Gestión de Incidentes</Text>
-    </View>
-  );
+  return <Login />;
 }
