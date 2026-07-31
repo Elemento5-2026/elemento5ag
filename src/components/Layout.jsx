db5854f1ff80d434e5b0e9e640836a1b61ccb17a
Elemento5-2026 <elemento5ag2026@outlook.com>
2026-07-31 14:38:20 -0600

    Refactor Layout component and import styles

---
+++ src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import "./Layout.css";
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+
+      <Sidebar />
+
+      <div className="layout-main">
+
+        <Header />
+
+        <main className="layout-content">
+          {children}
+        </main>
+
+      </div>
+
+    </div>
+  );
+}
